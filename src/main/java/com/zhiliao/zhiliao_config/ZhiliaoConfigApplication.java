59c08a0a90033396d7d818954e8b6786963f6e5e
pangde <18694054623@163.com>
2019-10-10 11:20:53 +0800

    1.zhiliao_config

---
+++ src/main/java/com/zhiliao/zhiliao_config/ZhiliaoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.zhiliao.zhiliao_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiliaoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiliaoConfigApplication.class, args);
+    }
+
+}
